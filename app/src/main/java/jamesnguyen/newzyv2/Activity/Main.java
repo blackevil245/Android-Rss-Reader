865b99d6d4ae018b57b364870c6641f849bea002
Hieu Nguyen <Blackevil245@gmail.com>
2015-09-25 15:57:52 +0300

    Implement Side Drawer

--- app/src/main/java/jamesnguyen/newzyv2/Activity/Main.java
+++ app/src/main/java/jamesnguyen/newzyv2/Activity/Main.java
@@ -3,29 +3,78 @@ package jamesnguyen.newzyv2.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import jamesnguyen.newzyv2.Fragments.RssFragment;
 import jamesnguyen.newzyv2.R;
 
 public class Main extends AppCompatActivity {
 
+    DrawerLayout mDrawerLayout;
+    ListView mDrawerList;
+    ActionBarDrawerToggle mDrawerToggle;
+    String[] mDrawerListItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /////// SETUP TOOLBAR ///////
+        /////// INIT VIEWS ///////
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mDrawerList = (ListView) findViewById(android.R.id.list);
+        mDrawerListItems = getResources().getStringArray(R.array.drawer_list);
+
+        /////// INIT TOOLBAR ///////
         EditText app_name = (EditText) findViewById(R.id.search_bar);
 
         setSupportActionBar(toolbar);
-        toolbar.setTitle("Newzy");
+        assert getSupportActionBar() != null;
+
+        /////// SETUP DRAWER ////////
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDrawerListItems));
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int editedPosition = position + 1;
+                Toast.makeText(Main.this, "You selected item " + editedPosition, Toast.LENGTH_SHORT).show();
+                mDrawerLayout.closeDrawer(mDrawerList);
+            }
+        });
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+                invalidateOptionsMenu();
+                syncState();
+            }
+
+            public void onDrawerClosed(View v) {
+                super.onDrawerClosed(v);
+                invalidateOptionsMenu();
+                syncState();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        /////// SETUP TOOLBAR /////////
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerToggle.syncState();
 
         /////// START RSS FRAGMENT //////
         if (savedInstanceState == null) {
@@ -39,6 +88,12 @@ public class Main extends AppCompatActivity {
         RssFragment fragment = new RssFragment();
         transaction.add(R.id.fragment_container, fragment);
         transaction.commit();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 
     @Override
